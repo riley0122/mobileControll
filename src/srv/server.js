e1c0ffdc2350bcee6aa65598699afda6e11da497
lucasammer <lucasammerlaan123@gmail.com>
2023-01-20 22:13:07 +0100

    added setup

--- src/srv/server.js
+++ src/srv/server.js
@@ -1,6 +1,7 @@
 const experss = require("express");
 const app = new experss();
 var ip = require('ip');
+const notifier = require('node-notifier');
 
 const port = 8000;
 
@@ -8,6 +9,10 @@ app.use(experss.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "\\clnt\\index.html");
+})
+
+app.get('/setup', (req, res) => {
+  res.sendFile(__dirname + "\\clnt\\setup.html");
 })
 
 app.listen(port, () => {
