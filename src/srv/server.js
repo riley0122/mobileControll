346866c5d8633b046917e61f06b453d45780702c
lucasammer <lucasammerlaan123@gmail.com>
2023-01-21 21:35:14 +0100

    added documentation

--- src/srv/server.js
+++ src/srv/server.js
@@ -1,38 +1,39 @@
 const experss = require("express");
-const app = new experss();
 var ip = require('ip');
 const notifier = require('node-notifier');
-const open = require('open');
-const fs = require("fs");
+const open = require('open');                            // Package for opening URL's
+const fs = require("fs");     
 
-const { port } = require('./config/ServerConfig.json');
+const app = new experss();                               // Initialises express
 
-app.use(experss.static('public'))
+const { port } = require('./config/ServerConfig.json');  // Get the port from the server config
+
+app.use(experss.static('public'))                        // Includes the public folder
 
 // standard pages
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "\\clnt\\index.html");
+  res.sendFile(__dirname + "\\clnt\\index.html");        // Sends the index file on the main page
 })
 
 app.get('/setup', (req, res) => {
-  res.sendFile(__dirname + "\\clnt\\setup.html");
+  res.sendFile(__dirname + "\\clnt\\setup.html");        // Sends the setup page on the main page
 })
 
 // saving settings in a file
 
 app.get('/api', async(req, res) => {
-  let path = 'config/UserSettings.json'
+  let path = 'config/UserSettings.json'                  // The relative path to the user settings
   switch (req.query.action) {
-    case "getAll":
+    case "getAll":                                       // Gets all user settings
       try {
         if (fs.existsSync(path)) {
-          fs.readFile(path, 'utf-8', (e, data) => {
+          fs.readFile(path, 'utf-8', (e, data) => {      // Read the file and send the parsed data back
             if(e) throw e;
             res.send(JSON.parse(data));
           })
         }else{
-          fs.writeFile(path, '{"exists":1}', (e) => {
+          fs.writeFile(path, '{"exists":1}', (e) => {    // If the file doesn't exist create it and set "exists" to 1
             if(e) throw e;
           })
         }
@@ -42,20 +43,20 @@ app.get('/api', async(req, res) => {
       }
       break;
       
-    case "set":
-       if(req.query.key!=""&&req.query.value!=""){
+    case "set":                                          // Sets the specified key to the specified value
+       if(req.query.key!=""&&req.query.value!=""){       // Check if both key & value are set
         if (fs.existsSync(path)) {
           fs.readFile(path, 'utf-8', (e, data) => {
             if(e) throw e;
             data = JSON.parse(data);
             data[req.query.key] = req.query.value;
-            fs.writeFile(path, JSON.stringify(data), (e) => {
+            fs.writeFile(path, JSON.stringify(data), (e) => { // write the new data with the new key to the file
               if(e) throw e;
             })
             res.send(data)
           })
         }else{
-          fs.writeFile(path, '{"error":"file doesnt exist"}', (e) => {
+          fs.writeFile(path, '{"error":"file doesnt exist"}', (e) => {  // If the file doesn't exist dont do anything
             if(e) throw e;
           })
         }
@@ -70,17 +71,17 @@ app.get('/api', async(req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`MobileControll server running on: http://${ip.address()}:${port}`)
-  notifier.notify({
+  console.log(`MobileControll server running on: http://${ip.address()}:${port}`) 
+  notifier.notify({                                        // Create a notification with the ip and port with the buttons to open setup and to open the page
     title: "MobileControll",
     message: `running on: http://${ip.address()}:${port}`,
     actions: ['open setup', 'open']
   })
 })
 
-notifier.on('open setup', () => {
+notifier.on('open setup', () => {                         // When the button to open setup is pressed it opens the server on the setup page
   open(`http://${ip.address()}:${port}/setup`)
 });
 notifier.on('open', () => {
-  open(`http://${ip.address()}:${port}`)
+  open(`http://${ip.address()}:${port}`)                  // When the open buttin is pressed it opens the site
 });
