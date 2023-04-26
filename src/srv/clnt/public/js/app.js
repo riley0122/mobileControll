f9da09f6843eeac3d5b91fe27f449be1b9f042f7
LucasAmmer <lucas@ammer.nl>
2023-04-26 22:02:05 +0200

    added auth input

---
+++ src/srv/clnt/public/js/app.js
@@ -0,0 +1,33 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+const saveAuth = () => {
+    try {
+        setCookie("auth", document.getElementById("auth").value, 31);
+        alert("saved!");
+    } catch (e) {
+        alert("something went wrong saving!");
+        console.error(e);
+    }
+}
+
+document.getElementById("auth").value = getCookie("auth");
