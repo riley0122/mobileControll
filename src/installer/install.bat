410473a5cc0d7a24d0cb5c7a164245e789261f9e
LucasAmmer <lucas@ammer.nl>
2023-04-27 16:19:25 +0200

    new installer and runner that (hopefully) work

---
+++ src/installer/install.bat
@@ -0,0 +1,18 @@
+@echo off
+
+where git >nul 2>&1
+if %errorlevel% neq 0 (
+  echo Error: Git is not installed. Please install Git and try again.
+  exit /b 1
+)
+
+cd %appdata%
+
+git clone https://github.com/lucasammer/mobileControll.git
+
+cd mobileControll\src\srv
+npm install
+
+schtasks /create /tn "mobilecontroll" /tr "%appdata%\LucasAmmer\mobilecontroll\src\srv\run.bat" /sc onevent /ec Microsoft-Windows-NetworkProfile/Operational /mo *[System[(EventID=10000)]]
+
+start run
---
+++ src/srv/run.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+where node >nul 2>&1
+if %ERRORLEVEL% equ 0 (
+  echo Node.js is installed
+  npm install
+  :loop
+    npm start
+    GOTO loop
+  GOTO loop
+) else (
+  echo Node.js is not installed!
+  pause
+  exit /b 1
+)
+
+endlocal
